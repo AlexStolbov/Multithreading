3bb8154f04c3eb078527709c707d0954b3073a8c
alexst <stolbov_alexey@mail.ru>
2018-06-05 15:14:30 +0300

    1. класс Count и метод int incremant() [#1102]

---
+++ MonitoreSynchronizy/src/main/java/Count.java
@@ -0,0 +1,17 @@
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class Count {
+    @GuardedBy("this")
+    private int value;
+
+    public synchronized void increment() {
+        this.value++;
+    }
+
+    public synchronized int getValue() {
+        return this.value;
+    }
+
+}
